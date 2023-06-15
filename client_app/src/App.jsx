be857d0814a974393bb026c1306e0d83c6ce665d
cla1976 <cla.assetech@gmail.com>
2023-06-14 23:19:19 -0300

    Add Reservas

--- client_app/src/App.jsx
+++ client_app/src/App.jsx
@@ -1,7 +1,9 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Navigation } from "./components/Navigation";
 import { Alumnos } from "./pages/Alumnos";
+import { Reservas } from "./pages/Reservas";
 import { FormAlumno } from "./pages/FormAlumno";
+import { FormReserva } from "./pages/FormReserva";
 import { Toaster } from "react-hot-toast";
 
 function App() {
@@ -15,6 +17,11 @@ function App() {
           <Route path="/alumnos" element={<Alumnos />} />
           <Route path="/alumnos/:id" element={<FormAlumno />} />
           <Route path="/alumnos-create" element={<FormAlumno />} />
+
+          <Route path="/" element={<Navigate to="/reservas" />} />
+          <Route path="/reservas" element={<Reservas />} />
+          <Route path="/reservas/:id" element={<FormReserva />} />
+          <Route path="/reservas-create" element={<FormReserva />} />
         </Routes>
         <Toaster />
       </div>
--- client_app/src/components/Navigation.jsx
+++ client_app/src/components/Navigation.jsx
@@ -4,7 +4,7 @@ export function Navigation() {
     return (
         <div className="flex justify-between py-3 items-center">
             <Link to="/alumnos">
-                <h1 className="font-bold text-3xl mb-4">Cliente App</h1>
+                <h1 className="font-bold text-3xl mb-4">Cliente Application</h1>
             </Link>
             <button className="bg-indigo-500 p-3 rounded-lg">
                 <Link to="/alumnos-create">Crear Alumno</Link>
